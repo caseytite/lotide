b03f7be84aa6d2419e29c2b85e5a0a1103cf68eb
Casey <caseyjtite@gmail.com>
2022-01-16 16:37:07 -0800

    refactor For recursion object array check

--- eqObjects.js
+++ eqObjects.js
@@ -13,6 +13,8 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 };
+//----------------------------------WITHOUT RECURSION
+/*
 const eqObjects = function (obj1, obj2) {
   // Returns true if both objects have identical keys with identical values.
   // Otherwise you get back a big fat false!
@@ -41,6 +43,39 @@ const eqObjects = function (obj1, obj2) {
 
   return true;
 };
+*/
+//----------------------------------------------WITH RECURSION
+
+const eqObjects = function (one, two) {
+  if (Object.keys(one).length !== Object.keys(two).length) return false;
+  for (let item in one) {
+    if (!Array.isArray(one[item]) && typeof one[item] === "object") {
+      return eqObjects(one[item], two[item]);
+    }
+    if (Array.isArray(one[item]) || Array.isArray(two[item])) {
+      if (one[item].length !== two[item].length) {
+        return false;
+      } else {
+        eqArrays(one[item], two[item]);
+      }
+    } else if (one[item] !== two[item]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(
+  eqObjects({ a: { z: [1, 2, 3] }, b: 2 }, { a: { z: [1, 2, 3] }, b: 2 })
+); // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+
+console.log(
+  eqObjects({ a: { z: 1, x: 15 }, b: 2 }, { a: { x: 15, z: 1 }, b: 2 })
+);
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 // console.log(eqObjects(ab, ba));
