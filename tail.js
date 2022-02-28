15f2e4c8e0910ff77ff266929f1a964553c947ad
Casey <caseyjtite@gmail.com>
2022-02-27 17:07:59 -0800

    remove comments

--- tail.js
+++ tail.js
@@ -1,8 +1,6 @@
-//
+
 
 const tail = function (arr) {
-  // console.log(arr.slice(1));
-  // return arr.slice(1);
   let output = arr.slice(1);
   return output;
 };
--- without.js
+++ without.js
@@ -1,19 +1,15 @@
 const without = function (source, itemsToRemove) {
   let newArray = [...source];
-  // let
-  // debugger;
   for (let i = 0; i < source.length; i++) {
     for (let x = 0; x < itemsToRemove.length; x++) {
       if (source[i] === itemsToRemove[x]) {
         newArray.splice(i, 1);
         break;
       } else {
-        // newArray.push(source[i]);
         continue;
       }
     }
   }
-  // console.log(newArray);
   return newArray;
 };
 module.exports = without;
